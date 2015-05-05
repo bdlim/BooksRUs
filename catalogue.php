3e10b316a12ef4d45701c16e84376482058ce4a2
Brandon Lim <bdlim2004@gmail.com>
2015-05-04 19:02:05 -0700

    Templated
    - Template Product Page

--- catalogue.php
+++ catalogue.php
@@ -15,6 +15,64 @@
 		<p>Our website is under construction, but below is our introduction!</p>
 		<h3>Introduction</h3>
 		<p>Books R Us is an online bookstore tailored to providing children with enjoyable books for them to read. We pride ourselves in providing excellent customer service and educational and inspiring literature for the children's young and developing minds. </p>
+		<form id="orderForm" action="" onclick="">
+			<table summary="catalogue">
+				<tr>
+					<th colspan="2">
+						Product
+					</th>
+					<th colspan="2">
+						Quantity
+					</th>
+					<th>
+						Price
+					</th>
+					<th>
+						Remove
+					</th>
+				</tr>
+				<tr>
+					<td>
+						<!-- PRODUCT IMAGE
+
+						<img src="*SOURCE*" alt="*NAME*" />
+
+						-->
+					</td>
+					<td>
+						<!-- PRODUCT NAME and DESCRIPTION
+
+						<h2>*NAME*</h2>
+						<p>*DESCRIPTION*<p>
+
+						-->
+					<td>
+						Quantity:
+					</td>
+					<td>
+						<!-- QUANTITY
+
+						<input type="text" name="*NAME*Quantity" size="3" />
+
+						-->
+					</td>
+					<td>
+						<!-- PRICE
+
+						*PRICE*
+
+						-->
+					</td>
+					<td>
+						<!-- REMOVE
+
+						<input type="checkbox" name="*NAME*Remove" value="no"/>
+
+						-->
+					</td>
+				</tr>
+			</table>
+		</form>
 	</div>
 	<!--#include file="footer.php"-->
 </body>
